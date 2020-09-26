1d4dfeaa43af015f3e73ee868ac14296144d0b9a
Calvin Cattaneo <calvin.cattaneo@gmail.com>
2020-09-26 08:20:09 -0300

    project

---
+++ Assets/Script/PegarItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PegarItem : MonoBehaviour {
+    public int Pontuacao;
+    public Text PontCont;
+
+    private void Update() {
+        PontCont.text = "X " + Pontuacao.ToString();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Moeda") {
+            Pontuacao += 10;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
